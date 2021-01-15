46b8d84f49107b6c94ca302546b9f4a584e390cd
Bogdan Stanciu <bogdan.stanciu@hmcts.net>
2021-01-15 09:22:50 +0000

    Add backend config to core

--- products/cft/provider.tf
+++ products/cft/provider.tf
@@ -6,3 +6,15 @@ provider "azurerm" {
     }
   }
 }
+
+terraform {
+  required_version = ">= 0.13.0"
+
+  backend "azurerm" {}
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "2.41.0"
+    }
+  }
+}
